87119cece84a58be4805cda8cd6f11c2fb248177
Dylan Powers <dylandpowers@gmail.com>
2023-04-11 22:12:02 -0400

    Add window listener

--- src/ShortcutType.ts
+++ src/ShortcutType.ts
@@ -1,3 +1,7 @@
 export const ShortcutType: Record<string, string> = {
   TAB_LATERAL: "TAB_LATERAL",
+  TAB_NUMBER: "TAB_NUMBER",
+  NEW_TAB: "NEW_TAB",
+  NEW_WINDOW: "NEW_WINDOW",
+  NEW_INCOGNITO_WINDOW: "NEW_INCOGNITO_WINDOW",
 };
--- src/background.ts
+++ src/background.ts
@@ -2,42 +2,68 @@ import { ShortcutType } from "./ShortcutType";
 
 let previousTab: chrome.tabs.Tab | null = null;
 
+// TABS
 chrome.tabs.onActivated.addListener(async (tab) => {
-  let silenced = await chrome.storage.local.get([ShortcutType.TAB_LATERAL]);
-  if (silenced[ShortcutType.TAB_LATERAL]) {
-    return;
-  }
+  // need to find if this is the only tab in the window, in which case don't send a notification
   const currentTab = await chrome.tabs.get(tab.tabId);
   if (previousTab !== null) {
     let indexDifference = currentTab.index - previousTab.index;
     if (Math.abs(indexDifference) === 1) {
       let shortcutText =
-        indexDifference < 0 ? "Command+Option+Left" : "Command+Option+Right";
+        indexDifference < 0
+          ? "Command + Option + Left"
+          : "Command + Option + Right";
       let direction = indexDifference < 0 ? "left" : "right";
-      createNotification(
+      createNotificationIfNotSilenced(
         ShortcutType.TAB_LATERAL,
         `To move tabs ${direction}, use ${shortcutText}`
       );
     } else {
-      createNotification(
-        ShortcutType.TAB_LATERAL,
-        "To jump to a specific tab, use Command+1 through Command+9"
+      createNotificationIfNotSilenced(
+        ShortcutType.TAB_NUMBER,
+        "To jump to a specific tab, use Command + 1 through Command + 8"
       );
     }
   }
   previousTab = currentTab;
 });
 
-chrome.notifications.onButtonClicked.addListener(
-  (notificationId, buttonIndex) => {
-    // there is only one button, so we don't need to check the index
-    chrome.storage.local.set({ [notificationId]: true });
-  }
-);
+chrome.tabs.onCreated.addListener(async () => {
+  createNotificationIfNotSilenced(
+    ShortcutType.NEW_TAB,
+    "To create a new tab, use Command + t"
+  );
+});
 
-function createNotification(notificationId: string, message: string) {
-  chrome.notifications.clear(notificationId);
-  chrome.notifications.create(notificationId, {
+// WINDOWS
+chrome.windows.onCreated.addListener(async (window) => {
+  if (window.incognito) {
+    createNotificationIfNotSilenced(
+      ShortcutType.NEW_INCOGNITO_WINDOW,
+      "To create a new incognito window, use Command + shift + n"
+    );
+  } else {
+    createNotificationIfNotSilenced(
+      ShortcutType.NEW_WINDOW,
+      "To create a new window, use Command + n"
+    );
+  }
+});
+
+chrome.notifications.onButtonClicked.addListener((notificationId) => {
+  // silence the notification
+  chrome.storage.local.set({ [notificationId]: true });
+});
+
+async function createNotificationIfNotSilenced(
+  shortcutType: string,
+  message: string
+) {
+  let silenced = await chrome.storage.local.get(shortcutType);
+  if (silenced[shortcutType]) {
+    return;
+  }
+  chrome.notifications.create({
     type: "basic",
     title: "Chrome Key Promoter",
     message,
